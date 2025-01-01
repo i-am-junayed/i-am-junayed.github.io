710185c1bc7fb3991b1af77aae54ce5a1a8bcb6d
i-am-junayed <junayed.ndc16@gmail.com>
2025-01-01 20:36:18 +0600

    Update publications.py

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -62,6 +62,49 @@ def html_escape(text):
 # In[5]:
 
 import os
+# for row, item in publications.iterrows():
+    
+#     md_filename = str(item.pub_date) + "-" + item.url_slug + ".md"
+#     html_filename = str(item.pub_date) + "-" + item.url_slug
+#     year = item.pub_date[:4]
+    
+#     ## YAML variables
+    
+#     md = "---\ntitle: \""   + item.title + '"\n'
+    
+#     md += """collection: publications"""
+    
+#     md += """\npermalink: /publication/""" + html_filename
+    
+#     if len(str(item.excerpt)) > 5:
+#         md += "\nexcerpt: '" + html_escape(item.excerpt) + "'"
+    
+#     md += "\ndate: " + str(item.pub_date) 
+    
+#     md += "\nvenue: '" + html_escape(item.venue) + "'"
+    
+#     if len(str(item.paper_url)) > 5:
+#         md += "\npaperurl: '" + item.paper_url + "'"
+    
+#     md += "\ncitation: '" + html_escape(item.citation) + "'"
+    
+#     md += "\n---"
+    
+#     ## Markdown description for individual page
+    
+#     # if len(str(item.paper_url)) > 5:
+#     #     md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n" 
+        
+#     if len(str(item.excerpt)) > 5:
+#         md += "\n" + html_escape(item.excerpt) + "\n"
+        
+#     # md += "\nRecommended citation: " + item.citation
+    
+#     md_filename = os.path.basename(md_filename)
+       
+#     with open("../_publications/" + md_filename, 'w') as f:
+#         f.write(md)
+
 for row, item in publications.iterrows():
     
     md_filename = str(item.pub_date) + "-" + item.url_slug + ".md"
@@ -70,11 +113,15 @@ for row, item in publications.iterrows():
     
     ## YAML variables
     
-    md = "---\ntitle: \""   + item.title + '"\n'
+    md = "---\ntitle: \"" + item.title + '"\n'
     
     md += """collection: publications"""
     
-    md += """\npermalink: /publication/""" + html_filename
+    # Use the paper URL for permalink if it exists
+    if len(str(item.paper_url)) > 5:
+        md += """\npermalink: """ + item.paper_url
+    else:
+        md += """\npermalink: /publication/""" + html_filename
     
     if len(str(item.excerpt)) > 5:
         md += "\nexcerpt: '" + html_escape(item.excerpt) + "'"
@@ -92,17 +139,16 @@ for row, item in publications.iterrows():
     
     ## Markdown description for individual page
     
-    # if len(str(item.paper_url)) > 5:
-    #     md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n" 
-        
     if len(str(item.excerpt)) > 5:
         md += "\n" + html_escape(item.excerpt) + "\n"
         
+    # Add recommended citation if needed
     # md += "\nRecommended citation: " + item.citation
     
     md_filename = os.path.basename(md_filename)
        
     with open("../_publications/" + md_filename, 'w') as f:
         f.write(md)
+
 
 
